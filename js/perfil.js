87b08e4790a4b1edf003c790650c615ae6d173d4
ferreirajoao-fatec <joao.ferreira113@fatec.sp.gov.br>
2025-04-17 08:49:11 -0300

    Pagina de perfil

---
+++ js/perfil.js
@@ -0,0 +1,23 @@
+var modal = document.getElementById("myModal");
+
+var btn1 = document.getElementById("cttsEmergencia");
+var btn2 = document.getElementById("cttsEmergencia");
+var btn3 = document.getElementById("cttsEmergencia");
+var btn4 = document.getElementById("cttsEmergencia");
+
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
