de6f48409f4736734c5df3ee53bf7918d96f95c7
ferreirajoao-cps <joao.ferreia406@etec.sp.gov.br>
2025-04-21 22:42:49 -0300

    Poucas Modificações perfil

--- js/perfil.js
+++ js/perfil.js
@@ -1,14 +1,14 @@
 var modal = document.getElementById("myModal");
 
 var btn1 = document.getElementById("cttsEmergencia");
-var btn2 = document.getElementById("cttsEmergencia");
-var btn3 = document.getElementById("cttsEmergencia");
-var btn4 = document.getElementById("cttsEmergencia");
+// var btn2 = document.getElementById("cttsEmergencia");
+// var btn3 = document.getElementById("cttsEmergencia");
+// var btn4 = document.getElementById("cttsEmergencia");
 
 
 var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
+btn1.onclick = function() {
   modal.style.display = "block";
 }
 
